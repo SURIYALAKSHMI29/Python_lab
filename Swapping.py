fe938260df1f72bd314a7a8ea00dbeb7463fcf99
SURIYALAKSHMI M <107531525+SURIYALAKSHMI29@users.noreply.github.com>
2022-12-25 19:55:17 +0530

    Update Swapping.py

--- Swapping.py
+++ Swapping.py
@@ -1,3 +1,4 @@
+Code 1:
 a=int(input("a="))
 b=int(input("b="))
 print("Before swaping,\na=",a ,"\nb=",b)
@@ -5,3 +6,15 @@ t=a
 a=b
 b=t
 print("After swapping,\na=",a,"\nb=",b)
+
+Code 2:
+a=int(input("a="))
+b=int(input("b="))
+print("Before swaping,\na=",a ,"\nb=",b)
+print("After swapping,\na=",b,"\nb=",a)
+
+Code 3:
+print("Enter a b=")
+a,b=map(int,input().split())
+print("Before swapping,",a,b)
+print("After swapping,",b,a)
